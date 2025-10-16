c9cd5243ecb6ef8f65cf92fbed9de2440e40f212
ToniBilic <abc>
2025-10-16 11:57:08 +0200

    update

--- Zadatak1/Zadatak1/zad1.c
+++ Zadatak1/Zadatak1/zad1.c
@@ -7,5 +7,69 @@ Napomena: Svaki redak datoteke sadrži ime i prezime studenta, te broj bodova na 
 relatvan_br_bodova = br_bodova/max_br_bodova*100
 */
 
+#define MAX_LINE (1024)
+#define _CRT_SECURE_NO_WARNINGS
+#define FILE_NOT_OPENED (-1)
+#define ERROR_MEMMORY_ALLOCATION (-2)
+#include <stdio.h>
+#include <stdlib.h>
 
+int studentNum(int i);
+
+typedef struct student {
+	char name[20];
+	char surname[20];
+	int pointsNum;
+}Student;
+
+int main() {
+	int k = 0, n = 0;
+	Student* s = NULL;
+	FILE* fp = NULL;
+	k = studentNum(n);
+	printf("There is %d students in the file.\n", k);
+
+	s = (Student*)malloc(k * sizeof(Student));  //alokacija memorije velicine brojStud
+	if (s == NULL) {
+		printf("Error while allocating memory!\n");
+		return ERROR_MEMMORY_ALLOCATION;
+	}
+
+	fp = fopen("Studenti.txt", "r");
+	if (fp == NULL) {
+		printf("Error while opening file!n");
+		return FILE_NOT_OPENED;
+	}
+
+	for (int z = 0; z < k; z++) {
+		fscanf(fp, "%s %s %d", s[z].name, s[z].surname, &s[z].pointsNum);
+	}
+
+	for (int i = 0; i < k - 1; i++) {
+		double relPoints = (s[i].pointsNum / 90.0) * 100; //relativni broj bodova
+		printf("Student: %s %s, has an absolute number of points: %d, and relative number of points: %.2f\n", s[i].name, s[i].surname, s[i].pointsNum, relPoints);
+	}
+	fp = fclose(fp);
+	free(s);
+
+	return 0;
+}
+
+int studentNum(int i) {     //fja za prebrojavanje studenata
+	FILE* fp;
+	char buffer[MAX_LINE];
+	fp = fopen("Studenti.txt", "r");
+
+	if (fp == NULL) {
+		printf("Greska pri otvaranju datoteke.\n");
+		return FILE_NOT_OPENED;
+	}
+	else {
+		while (fgets(buffer, MAX_LINE, fp) != NULL) {
+			i = i + 1;
+		}
+		fp = fclose(fp);
+		return i;
+	}
+}
 
